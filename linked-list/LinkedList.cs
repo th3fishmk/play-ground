cc863227587af9fdcfe7a042e1d7e373c6f6ce77
Fish <th3fishmk@proton.me>
2026-05-17 22:40:10 -0600

    created a linked list

--- linked-list/LinkedList.cs
+++ linked-list/LinkedList.cs
@@ -1,12 +1,15 @@
+using System.Linq.Expressions;
+
 namespace Csharp
 {
     public class MyLinkedList<T>
+        where T : IComparable<T>
     {
-        public Node<T>? head;
+        private Node? head;
 
         public void Append(T value)
         {
-            var newNode = new Node<T>(value);
+            var newNode = new Node(value);
 
             if (head == null)
             {
@@ -37,10 +40,41 @@ namespace Csharp
             // move the head to a new variable
             // create a new head
             // point the head to the new variable
-            var newHead = new Node<T>(value);
+            var newHead = new Node(value);
             var oldHead = head;
             head = newHead;
             head.Next = oldHead;
+        }
+
+        public int Size()
+        {
+            var steps = 0;
+            var next = head;
+            while (next != null)
+            {
+                steps++;
+                next = next.Next;
+            }
+            return steps;
+        }
+
+        public T? Head()
+        {
+            return head!.Value;
+        }
+
+        public T Tail()
+        {
+            if (head == null)
+            {
+                throw new Exception("Trying to access an empty linked list");
+            }
+            var current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            return current.Value;
         }
 
         public void PrintAllValues()
@@ -60,11 +94,55 @@ namespace Csharp
                 }
             }
         }
-    }
 
-    public class Node<T>(T value)
-    {
-        public T Value = value;
-        public Node<T>? Next = null;
+        public T AtIndex(int index)
+        {
+            if (index < 0 | head == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current!.Next;
+            }
+            return current!.Value;
+        }
+
+        public T Pop()
+        {
+            if (head == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                var returning = head;
+                head = returning.Next;
+
+                return returning.Value;
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            var next = head;
+            while (next != null)
+            {
+                if (next.Value.Equals(value))
+                {
+                    return true;
+                }
+                next = next.Next;
+            }
+            return false;
+        }
+
+        class Node(T value)
+        {
+            public T Value = value;
+            public Node? Next;
+        }
     }
 }
--- linked-list/Program.cs
+++ linked-list/Program.cs
@@ -7,8 +7,6 @@ stringList.Append("Hello ");
 stringList.Append("world");
 stringList.Append("!");
 
-stringList.PrintAllValues();
-
 var numberList = new MyLinkedList<int>();
 
 numberList.Append(1);
@@ -17,8 +15,41 @@ numberList.Append(3);
 numberList.Append(4);
 numberList.Append(5);
 
-numberList.PrintAllValues();
-
+WriteLine($"The head value is: {numberList.Head()}");
 numberList.Prepend(0);
+WriteLine($"The head value is: {numberList.Head()}");
 
-numberList.PrintAllValues();
+WriteLine($"Total size of {nameof(numberList)} is: {numberList.Size()}");
+
+WriteLine($"The tail for {nameof(numberList)} is {numberList.Tail()}");
+
+var nullList = new MyLinkedList<int>();
+
+try
+{
+    WriteLine($"The tail for {nameof(nullList)} is {nullList.Tail()}");
+}
+catch (System.Exception e)
+{
+    WriteLine(e.Message);
+}
+
+var index = stringList.Size() - 1;
+
+try
+{
+    WriteLine($"index: {index} of {nameof(stringList)}: {stringList.AtIndex(index)}");
+}
+catch (Exception e)
+{
+    WriteLine(e.Message);
+}
+
+WriteLine($"{nameof(stringList)} before pop:");
+stringList.PrintAllValues();
+stringList.Pop();
+WriteLine($"{nameof(stringList)} after pop:");
+stringList.PrintAllValues();
+
+WriteLine($"Is 5 in {nameof(nullList)}? {nullList.Contains(5)}");
+nullList.PrintAllValues();
